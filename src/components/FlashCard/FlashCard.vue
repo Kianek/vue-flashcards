18709434b6ca3f0094457c0e4782e6ad8fbf824a
Keanu Allridge <kallridge92@gmail.com>
2019-08-21 06:32:03 -0700

    Initial commit

---
+++ src/components/FlashCard/FlashCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-card class="flash-card">
+    <div class="content">
+      <h2 class="mt-2 text-center">{{ subject }}</h2>
+      <div class="front" v-if="!flipped">
+        <v-card-text>{{ question }}</v-card-text>
+        <v-card-text v-if="showHint">Hint: {{ hint }}</v-card-text>
+      </div>
+      <div class="back" v-else>
+        <v-card-text>{{ answer }}</v-card-text>
+      </div>
+
+      <v-card-actions class="btn-group">
+        <v-btn
+          class="mb-2"
+          block
+          depressed
+          @click="showHint = !showHint"
+          v-if="!flipped && hint"
+        >hint?</v-btn>
+        <v-btn class="ml-0" block depressed @click="flipped = !flipped">flip</v-btn>
+      </v-card-actions>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    subject: String,
+    question: String,
+    answer: String,
+    hint: String
+  },
+  data() {
+    return {
+      flipped: false,
+      showHint: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.flash-card {
+  height: 275px;
+  width: 350px;
+  perspective: 1000px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
+
+.front,
+.back {
+  width: 100%;
+}
+
+.btn-group {
+  margin-top: auto;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+// .back {
+//   position: absolute;
+//   width: 100%;
+//   height: 100%;
+//   backface-visibility: hidden;
+//   transform: rotateY(180deg);
+// }
+
+// .flip-card {
+//   transform: rotateY(180deg);
+// }
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <FlashCard
+      subject="Animals"
+      question="Which animal goes 'woof'?"
+      answer="Doggos!"
+      hint="Often called Fido"
+    />
+  </div>
+</template>
+
+<script>
+import FlashCard from "../components/FlashCard/FlashCard";
+
+export default {
+  name: "dashboard",
+  components: {
+    FlashCard
+  }
+};
+</script>
